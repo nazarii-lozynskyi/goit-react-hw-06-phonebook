72df8cec4071e06e520917106189ec92465c3ec2
Nazarii Lozynskyi <nazarko.l.m@gmail.com>
2021-11-08 10:31:46 +0100

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,7 +1,12 @@
 import React from "react";
 import ReactDOM from "react-dom";
+
+import store from "./redux/store";
+
 import "./index.css";
 import App from "./App";
+
+console.log(store);
 
 ReactDOM.render(
   <React.StrictMode>
