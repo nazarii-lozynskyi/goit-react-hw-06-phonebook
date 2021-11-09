b63f612301b17e7c34f242270d8bc5c426a5a560
Nazarii Lozynskyi <nazarko.l.m@gmail.com>
2021-11-09 04:37:10 +0100

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,20 +1,78 @@
-import { Container } from "@mui/material";
-import "./App.css";
+import { useState, useEffect } from "react";
+
+import { v4 as uuidv4 } from "uuid";
+
+import { ToastContainer, toast } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
+
 import Contacts from "./components/Contacts";
 import Form from "./components/Form/Form";
 import Header from "./components/Header";
 import SearchContactForm from "./components/SearchContactForm";
 
+import { Container } from "@mui/material";
+import "./App.css";
+
 function App() {
+  const [contacts, setContacts] = useState(
+    () => JSON.parse(localStorage.getItem("Contacts")) ?? []
+  );
+  const [filter, setFilter] = useState("");
+
+  useEffect(() => {
+    localStorage.setItem("Contacts", JSON.stringify(contacts));
+  }, [contacts]);
+
+  const addContacts = (data) => {
+    const contact = {
+      id: uuidv4(),
+      ...data,
+    };
+
+    const checkForDuplicationOfContacts = contacts.find(
+      (contact) => contact.name.toLowerCase() === data.name.toLowerCase()
+    );
+
+    if (checkForDuplicationOfContacts) {
+      toast.error(`${data.name} is already in contacts`);
+    } else {
+      setContacts((prevState) => [contact, ...prevState]);
+    }
+  };
+
+  const deleteContact = (contactId) => {
+    setContacts(contacts.filter((contact) => contact.id !== contactId));
+  };
+
+  const changeFilter = (event) => {
+    setFilter(event.currentTarget.value.toLowerCase());
+  };
+
+  const normalizedFilter = filter.toLowerCase();
+  const visibleContacts = contacts.filter((contact) =>
+    contact.name.toLowerCase().includes(normalizedFilter)
+  );
+
   return (
     <>
       <Header />
       <Container>
-        <Form />
+        <Form onSubmit={addContacts} />
 
-        <SearchContactForm />
+        <SearchContactForm onChange={changeFilter} value={filter} />
 
-        <Contacts />
+        <ToastContainer
+          theme="colored"
+          position="top-right"
+          autoClose={3000}
+          hideProgressBar={false}
+          closeOnClick={true}
+          pauseOnHover={true}
+          draggable={true}
+          progress={undefined}
+        />
+
+        <Contacts contacts={visibleContacts} onDeleteContact={deleteContact} />
       </Container>
     </>
   );
