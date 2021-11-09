417fbfc1ac6caf085af0c862b9bb1e579d31f34c
Nazarii Lozynskyi <nazarko.l.m@gmail.com>
2021-11-09 02:23:35 +0100

    Update Contacts.jsx

--- src/components/Contacts/Contacts.jsx
+++ src/components/Contacts/Contacts.jsx
@@ -14,7 +14,50 @@ import styles from "./Contact.module.css";
 export default function Contacts() {
   return (
     <List sx={{ bgcolor: "background.paper" }} className={styles.list}>
-      <ListItem>
+      <ListItem
+        sx={{
+          borderColor: "primary.main",
+          border: 1,
+          borderRadius: 2,
+        }}
+        className={styles.item}
+      >
+        <ListItemAvatar>
+          <Avatar sx={{ bgcolor: "primary.dark" }}>
+            <AccountBox />
+          </Avatar>
+        </ListItemAvatar>
+        <ListItemText
+          primary="Nameggggggggg kjkjkjkjkjkjkjk"
+          secondary="Numberhhhhhhhhhhhhhhhhh"
+        />
+        <IconButton edge="end" aria-label="delete" sx={{ marginLeft: "40px" }}>
+          <Delete sx={{ color: "error.main" }} />
+        </IconButton>
+      </ListItem>
+
+      <ListItem
+        sx={{ borderColor: "primary.main", border: 1, borderRadius: 2 }}
+        className={styles.item}
+      >
+        <ListItemAvatar>
+          <Avatar sx={{ bgcolor: "primary.dark" }}>
+            <AccountBox />
+          </Avatar>
+        </ListItemAvatar>
+        <ListItemText
+          primary="Nameggggggggg kjkjkjkjkjkjkjk"
+          secondary="Numberhhhhhhhhhhhhhhhhh"
+        />
+        <IconButton edge="end" aria-label="delete" sx={{ marginLeft: "40px" }}>
+          <Delete sx={{ color: "error.main" }} />
+        </IconButton>
+      </ListItem>
+
+      <ListItem
+        sx={{ borderColor: "primary.main", border: 1, borderRadius: 2 }}
+        className={styles.item}
+      >
         <ListItemAvatar>
           <Avatar sx={{ bgcolor: "primary.dark" }}>
             <AccountBox />
