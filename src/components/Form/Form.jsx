ba0b28663ae0a8cca4653e320c07877af2e33af6
Nazarii Lozynskyi <nazarko.l.m@gmail.com>
2021-11-09 04:37:22 +0100

    Update Form.jsx

--- src/components/Form/Form.jsx
+++ src/components/Form/Form.jsx
@@ -1,16 +1,31 @@
+import { useState } from "react";
+
 import { AccountCircle, AddIcCall, LocalPhone } from "@mui/icons-material";
 import { Button, TextField, Tooltip } from "@mui/material";
 import { Box } from "@mui/system";
 
-export default function Form() {
+export default function Form({ onSubmit }) {
+  const [state, setState] = useState({ name: "", number: "" });
+
+  const handleChange = (event) => {
+    const { name, value } = event.currentTarget;
+    setState((prevState) => ({ ...prevState, [name]: value }));
+  };
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+
+    onSubmit(state);
+
+    reset();
+  };
+
+  const reset = () => {
+    setState({ name: "", number: "" });
+  };
+
   return (
-    <Box
-      sx={{ marginTop: "20px" }}
-      component="form"
-      onSubmit={(e) => {
-        e.preventDefault();
-      }}
-    >
+    <Box sx={{ marginTop: "20px" }} component="form" onSubmit={handleSubmit}>
       <Tooltip title="Enter name" placement="left">
         <Box
           sx={{
@@ -26,7 +41,13 @@ export default function Form() {
             id="input-Name"
             label="Name"
             variant="standard"
-            type="search"
+            type="text"
+            name="name"
+            pattern="^[a-zA-Zа-яА-Я]+(([' -][a-zA-Zа-яА-Я ])?[a-zA-Zа-яА-Я]*)*$"
+            title="Имя может состоять только из букв, апострофа, тире и пробелов. Например Adrian, Jacob Mercer, Charles de Batz de Castelmore d'Artagnan и т. п."
+            required
+            value={state.name}
+            onChange={handleChange}
           />
         </Box>
       </Tooltip>
@@ -45,7 +66,13 @@ export default function Form() {
             id="input-Phone"
             label="Phone"
             variant="standard"
-            type="search"
+            type="tel"
+            name="number"
+            pattern="\+?\d{1,4}?[-.\s]?\(?\d{1,3}?\)?[-.\s]?\d{1,4}[-.\s]?\d{1,4}[-.\s]?\d{1,9}"
+            title="Номер телефона должен состоять цифр и может содержать пробелы, тире, круглые скобки и может начинаться с +"
+            required
+            value={state.number}
+            onChange={handleChange}
           />
         </Box>
       </Tooltip>
