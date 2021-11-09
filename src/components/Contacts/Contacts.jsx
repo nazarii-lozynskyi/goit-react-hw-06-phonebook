e62186a7f253ebf260af0437d151619b46816382
Nazarii Lozynskyi <nazarko.l.m@gmail.com>
2021-11-09 02:06:35 +0100

    Update Contacts.jsx

--- src/components/Contacts/Contacts.jsx
+++ src/components/Contacts/Contacts.jsx
@@ -1,26 +1,32 @@
-import { AccountBox } from "@mui/icons-material";
+import { AccountBox, Delete } from "@mui/icons-material";
 import {
   Avatar,
   Grid,
+  IconButton,
   List,
   ListItem,
   ListItemAvatar,
   ListItemText,
 } from "@mui/material";
 
+import styles from "./Contact.module.css";
+
 export default function Contacts() {
   return (
-    <List sx={{ width: "100%", maxWidth: 360, bgcolor: "background.paper" }}>
+    <List sx={{ bgcolor: "background.paper" }} className={styles.list}>
       <ListItem>
         <ListItemAvatar>
-          <Avatar>
+          <Avatar sx={{ bgcolor: "primary.dark" }}>
             <AccountBox />
           </Avatar>
         </ListItemAvatar>
         <ListItemText
-          primary="Nameggggggggggggggggggggggggggg"
-          secondary="Numberhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhh"
+          primary="Nameggggggggg kjkjkjkjkjkjkjk"
+          secondary="Numberhhhhhhhhhhhhhhhhh"
         />
+        <IconButton edge="end" aria-label="delete" sx={{ marginLeft: "40px" }}>
+          <Delete sx={{ color: "error.main" }} />
+        </IconButton>
       </ListItem>
     </List>
   );
