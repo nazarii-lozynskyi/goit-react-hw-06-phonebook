b77b1bec39882c9545ddc297eda9ed0a42e2b9a0
Nazarii Lozynskyi <nazarko.l.m@gmail.com>
2021-11-09 01:18:18 +0100

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,7 +1,23 @@
+import { Container } from "@mui/material";
 import "./App.css";
+import Contacts from "./components/Contacts";
+import Form from "./components/Form/Form";
+import Header from "./components/Header";
+import SearchContactForm from "./components/SearchContactForm";
 
 function App() {
-  return <p>aloe</p>;
+  return (
+    <>
+      <Header />
+      <Container>
+        <Form />
+
+        <SearchContactForm />
+
+        <Contacts />
+      </Container>
+    </>
+  );
 }
 
 export default App;
