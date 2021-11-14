b8a675faabb38d8ea0ccfe9199e9276c1418b985
Nazarii Lozynskyi <nazarko.l.m@gmail.com>
2021-11-14 22:58:38 +0100

    Update store.js

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,10 +1,32 @@
+import {
+  persistStore,
+  persistReducer,
+  FLUSH,
+  REHYDRATE,
+  PAUSE,
+  PERSIST,
+  PURGE,
+  REGISTER,
+} from "redux-persist";
+import storage from "redux-persist/lib/storage"; // defaults to localStorage for web
+
 //import { createStore, combineReducers } from "redux";
-import { configureStore } from "@reduxjs/toolkit";
+import { configureStore, combineReducers } from "@reduxjs/toolkit";
 
 import logger from "redux-logger";
 //import { composeWithDevTools } from "redux-devtools-extension";
 
 import contactsReducer from "./phonebook/phonebook-reducer";
+
+const contactsPersistConfig = {
+  key: "Contacts",
+  storage,
+  blacklist: ["filter"],
+};
+
+const rootReducer = combineReducers({
+  contacts: persistReducer(contactsPersistConfig, contactsReducer),
+});
 
 //const rootReducer = combineReducers({
 //  contacts: contactsReducer,
@@ -13,11 +35,16 @@ import contactsReducer from "./phonebook/phonebook-reducer";
 //const store = createStore(rootReducer, composeWithDevTools());
 
 const store = configureStore({
-  reducer: {
-    contacts: contactsReducer,
-  },
+  reducer: rootReducer,
   devTools: process.env.NODE_ENV === "development",
-  middleware: (getDefaultMiddleware) => getDefaultMiddleware().concat(logger),
+  middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware({
+      serializableCheck: {
+        ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER],
+      },
+    }).concat(logger),
 });
 
-export default store;
+const persistor = persistStore(store);
+
+export default { store, persistor };
