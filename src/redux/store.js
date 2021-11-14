7e53f21cad4bc2df21483c462badf9383dce3bfc
Nazarii Lozynskyi <nazarko.l.m@gmail.com>
2021-11-14 21:53:24 +0100

    Update store.js

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,6 +1,7 @@
 //import { createStore, combineReducers } from "redux";
 import { configureStore } from "@reduxjs/toolkit";
 
+import logger from "redux-logger";
 //import { composeWithDevTools } from "redux-devtools-extension";
 
 import contactsReducer from "./phonebook/phonebook-reducer";
@@ -16,6 +17,7 @@ const store = configureStore({
     contacts: contactsReducer,
   },
   devTools: process.env.NODE_ENV === "development",
+  middleware: (getDefaultMiddleware) => getDefaultMiddleware().concat(logger),
 });
 
 export default store;
