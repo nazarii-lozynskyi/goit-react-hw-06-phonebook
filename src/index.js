26997b72e1b577ee71aeb2f3b3a6f53f8f78ce06
Nazarii Lozynskyi <nazarko.l.m@gmail.com>
2021-11-14 19:43:40 +0100

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,18 +1,17 @@
 import React from "react";
 import ReactDOM from "react-dom";
 
+import { Provider } from "react-redux";
 import store from "./redux/store";
-
-import { myAction } from "./redux/actions";
 
 import "./index.css";
 import App from "./App";
 
-console.log(store);
-
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Provider store={store}>
+      <App />
+    </Provider>
   </React.StrictMode>,
   document.getElementById("root")
 );
