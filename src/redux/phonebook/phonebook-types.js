8f31aaf6d428ace4fb0f0f1091f9231ea51f0e18
Nazarii Lozynskyi <nazarko.l.m@gmail.com>
2021-11-14 19:43:58 +0100

    Create phonebook-types.js

---
+++ src/redux/phonebook/phonebook-types.js
@@ -0,0 +1,8 @@
+const actions = {
+  ADD: "contacts/add",
+  DELETE: "contacts/delete",
+  TOGGLE_COMPLETED: "contacts/toggleCompleted",
+  CHANGE_FILTER: "contacts/ChangeFilter",
+};
+
+export default actions;
